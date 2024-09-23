7e9949663cc1d092d82065ab952e6e7873c0fa93
waqarahmad30 <wa.ahmad1999@gmail.com>
2024-09-23 14:32:07 +0500

    deployment stage changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import { AppComponent } from './app/app.component';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import'font-awesome/css/font-awesome.min.css';
+// import'font-awesome/css/font-awesome.min.css';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
