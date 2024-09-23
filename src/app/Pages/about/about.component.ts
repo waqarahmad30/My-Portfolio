f615d5b536621d277e78c7c50f5a5d14dd32bfea
waqarahmad30 <wa.ahmad1999@gmail.com>
2024-09-23 12:18:14 +0500

    complete all the tasks

--- src/app/Pages/about/about.component.ts
+++ src/app/Pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SkillSliderComponent } from '../../Reuseable/skill-slider/skill-slider.component';
+import { ContactSecComponent } from '../../Reuseable/contact-sec/contact-sec.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [SkillSliderComponent],
+  imports: [SkillSliderComponent,ContactSecComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
